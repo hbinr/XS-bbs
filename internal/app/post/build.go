631318a156e0d1e4bb2612af7f5b6538d4509b93
dhb <duanhaobin@drcode.com>
2022-08-07 09:38:11 +0800

    upgrade: go-redis version

--- internal/app/post/build.go
+++ internal/app/post/build.go
@@ -2,7 +2,7 @@ package post
 
 import (
 	"github.com/gin-gonic/gin"
-	"github.com/go-redis/redis"
+	"github.com/go-redis/redis/v9"
 	"gorm.io/gorm"
 
 	"xs.bbs/internal/app/post/controller"
@@ -24,17 +24,19 @@ var (
 )
 
 /*
-	笔记：
-	曾经想 service.NewPostService()修改为service.NewService(store)
+笔记：
+曾经想 service.NewPostService()修改为service.NewService(store)
 
-	其中store是一个接口，嵌套了全部数据访问接口: PostRepo + userRepo + communityRepo
+其中store是一个接口，嵌套了全部数据访问接口: PostRepo + userRepo + communityRepo
 
-	然后service层可以链式调用：
-		c.repository.Post().Find(args1)....
-		c.repository.User().Create(args2)....
-		c.repository.Community().Update(args3)....
+然后service层可以链式调用：
 
-	但是后来否决了这个想法，原因如下：
+	c.repository.Post().Find(args1)....
+	c.repository.User().Create(args2)....
+	c.repository.Community().Update(args3)....
+
+但是后来否决了这个想法，原因如下：
+
 		1.我们应该给函数传入它关心的最小集合作为参数，而不是，我有一个 struct，当某个函数需要这个 struct 的成员的时候，
 	    我们把整个 struct 都作为参数传递进去。应该仅仅传递函数关心的最小集合。
 	    2.不要链式调用方法，传进去的一整条调用链对函数来说，都是无关的耦合，只会让代码更 hard to change，让工程师惧怕去修改
